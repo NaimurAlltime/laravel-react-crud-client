e1d0035dc1b034d9eb5d9d3ba19d980a8b5b1396
Naimur Rahman <naimur2231@gmail.com>
2023-08-01 16:26:54 +0600

    laravel and react crud operation

---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About/About";
+import Blog from "./components/Blog/Blog";
+import Contact from "./components/Contact/Contact";
+import Home from "./components/Home/Home";
+import "./index.css";
+import Main from "./layout/Main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <div className="max-w-screen-2xl mx-auto">
+      <RouterProvider router={router} />
+    </div>
+  </React.StrictMode>
+);
